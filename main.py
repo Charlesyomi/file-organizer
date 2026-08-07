d8d12bcf94c84e38c48627b53111655e85c46417
Charles Adesoba <yomiadesoba@gmail.com>
2026-08-07 23:23:42 +0100

    Wire up argparse with --source and --dry-run support.

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from pathlib import Path
+import argparse
 from utils import scan_directory, move_file,classify_file
 
-def organize_directory(source:Path):
+def organize_directory(source:Path, dry_run : bool = False):
     """
     Organizes the files in the given source directory into subdirectories based on their file extensions.
 
@@ -17,20 +18,36 @@ def organize_directory(source:Path):
     if not files:
         print(f"No files exist in the directory '{source.name}' to organize")
 
+    if dry_run:
+        print()
+
     for file in files:
         category = classify_file(file)
-        
-        category_folder = source / category
 
-        # move file by design already implicitly creates the category folder if it does not exist
+        if dry_run:
+            print(f"[DRY RUN] {file.name} -> {category}/")
 
-        try:
-            move_file(file,category_folder)
-        except (PermissionError, FileExistsError, FileNotFoundError) as e:
-            print(f'Error occured in file "{file.name}" : {e}')
+        else:       
+            category_folder = source / category
+
+            # move file by design already implicitly creates the category folder if it does not exist
+
+            try:
+                move_file(file,category_folder)
+            except (PermissionError, FileExistsError, FileNotFoundError) as e:
+                print(f'Error occured in file "{file.name}" : {e}')
+
+
 
 if __name__ == "__main__":
-    organize_directory(Path.home() / "projects" / "file-organizer" / "scratch_test")
+    parser = argparse.ArgumentParser(description="Organize files by type.")
+
+    parser.add_argument("--source", required=True, help="Directory to organize")
+    parser.add_argument("--dry-run", action="store_true", help="Preview without moving files")
+
+    args = parser.parse_args()
+    
+    organize_directory(source=Path(args.source), dry_run=args.dry_run)
 
 
 
